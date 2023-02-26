fd10794e7e4b3b52988a31c10a7601dc51954bb8
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-02-26 11:53:31 +0530

    commit

--- script.js
+++ script.js
@@ -7,9 +7,16 @@ function swap(){
     //body.style.backgroundcolor="dark";
     // var element = document.body;
     // element.classList.toggle("dark-mode");
+    if(button.getAttribute("class")==="button_day"){
     document.body.style.backgroundColor = "dark";
     button.setAttribute("class", "button_night")
     main.setAttribute("class","night")
+    }
+    else{
+        document.body.style.backgroundColor = "light";
+        button.setAttribute("class", "button_day")
+        main.setAttribute("class","day") 
+    }
 
     //console.log("hi")
 }
