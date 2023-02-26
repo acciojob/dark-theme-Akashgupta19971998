6682af6aa6e4025498153e3bbd3a5ad21e4163bf
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-02-26 11:27:06 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your code here
 let button=document.getElementById('swap');
 let body=document.getElementsByTagName('body')
+let main=document.getElementById("app")
 
 function swap(){
     //body.style.backgroundcolor="dark";
     document.body.style.backgroundColor = "dark";
-    console.log("hi")
+    button.setAttribute(button_day, button_night)
+    main.setAttribute(day,night)
+
+    //console.log("hi")
 }
 
